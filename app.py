885e55cece3766adc649f8e9c98c52a7f7c2caf8
davekim-dev <davekim695@gmail.com>
2026-04-09 20:37:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+# cat 명령어로 파일 만들기
+cat > app.py << 'EOF'
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+EOF
+
+cat app.py로 확인
+결과
 from flask import Flask
 
 app = Flask(__name__)
